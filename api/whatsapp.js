178b582f88f8ec3e8a56b0a690dafdc0241671be
ventas281 <ventas@rubensdistribuidora.com>
2026-06-06 14:21:02 -0600

    Add Google Tag Manager to whatsapp.js
    Added Google Tag Manager script for tracking.

---
+++ api/whatsapp.js
@@ -0,0 +1,12 @@
+<head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18217813092"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'AW-18217813092');
+  </script>
+
+  <meta charset="UTF-8">
+  ... resto de tu código ...
+</head>
