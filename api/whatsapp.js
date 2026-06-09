d06e7eefcd1de7a4b5a7461dd8bf8e9db887f877
Brandon <martinezgabriela1973@gmail.com>
2026-06-08 19:59:16 -0600

    Google 2

--- api/whatsapp.js
+++ api/whatsapp.js
@@ -1,12 +1,1 @@
-<head>
-  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18217813092"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'AW-18217813092');
-  </script>
 
-  <meta charset="UTF-8">
-  ... resto de tu código ...
-</head>
